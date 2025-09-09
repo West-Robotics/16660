a7d2167cfd503112093999d1d0f8ed14f048b075
Pancraciolego <154642672+Pancraciolego@users.noreply.github.com>
2025-09-08 20:54:05 -0800

    kot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/setup/motorSetup.kt
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.setup
+
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorEx
+import com.qualcomm.robotcore.hardware.DcMotorSimple
+import com.qualcomm.robotcore.hardware.HardwareMap
+
+class motorSetup (hardwareMap: HardwareMap,name:String, direction: DcMotorSimple.Direction,zeroPower: DcMotor.ZeroPowerBehavior){
+
+    val motor = hardwareMap.dcMotor.get(name) as DcMotorEx
+
+    init {
+        motor.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        motor.direction = direction
+        motor.zeroPowerBehavior = zeroPower
+    }
+
+
+}
