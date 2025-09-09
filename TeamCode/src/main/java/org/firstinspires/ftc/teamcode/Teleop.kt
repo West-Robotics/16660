3926e6ee59c1278aeb984d32babd039d948aceb0
Pancraciolego <onofrealvaradoraziel@gmail.com>
2025-09-08 22:54:52 -0800

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.kt
@@ -4,15 +4,20 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.teamcode.setup.Controller;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import org.firstinspires.ftc.teamcode.mechanisms.drivetrain
 
 @TeleOp
 class Teleop: LinearOpMode(){
 
 
     override fun runOpMode(){
-        val gamepad: Gamepad = Gamepad()
+        val drive = drivetrain()
 
-        val controller: Controller = Controller(gamepad)
+        waitForStart()
+
+        while (opModeIsActive()){
+
+        }
     }
 
 
