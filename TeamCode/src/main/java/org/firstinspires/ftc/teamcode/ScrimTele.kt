abb64e823aee0af182ca09361a9324f71b6f0818
Pancraciolego <onofrealvaradoraziel@gmail.com>
2025-10-25 15:23:43 -0800

    finit state machine
    yaaaay

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScrimTele.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScrimTele.kt
@@ -3,20 +3,22 @@ package org.firstinspires.ftc.teamcode
 import com.qualcomm.hardware.lynx.LynxModule
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.Servo
+import com.qualcomm.robotcore.util.ElapsedTime
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit
 import org.firstinspires.ftc.teamcode.mechanisms.drivetrain
 import org.firstinspires.ftc.teamcode.mechanisms.flywheels
 import org.firstinspires.ftc.teamcode.setup.Controller
-import com.qualcomm.robotcore.hardware.Servo
-import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit
 
 
 @TeleOp(name="ScrimTeleOp1Controller")
 class ScrimTele : LinearOpMode(){
 
     enum class OutTake {
-        OUTTAKE_START,
+        OUTTAKE_OPEN,
+        OUTTAKE_CLOSED,
+        RUN_FLY_MOTORS,
         PUSH_SERVO,
-        INTAKE_OUT
     }
 
     override fun runOpMode() {
@@ -25,15 +27,65 @@ class ScrimTele : LinearOpMode(){
         val drive = drivetrain(hardwareMap, false)
         val flywheels = flywheels(hardwareMap)
         val controller1 = Controller(gamepad1)
-        var outTake: OutTake = OutTake.OUTTAKE_START
+        var outTake: OutTake = OutTake.OUTTAKE_OPEN
+        var timer = ElapsedTime()
         waitForStart()
         val allHubs = hardwareMap.getAll<LynxModule?>(LynxModule::class.java)
         for (module in allHubs) {
-            module.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO)
+            module.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL)
         }
+        timer.reset()
         while (opModeIsActive()) {
+            for (hub in allHubs) {
+                hub.clearBulkCache()
+            }
             controller1.update()
 
+            drive.stickTankEffort(controller1.right_stick_x, -controller1.left_stick_y)
+            flywheels.runMotors()
+            drive.runMotors()
+
+            when (outTake) {
+                OutTake.OUTTAKE_OPEN ->{
+                    intake.position = 0.25
+                    intake2.position = 0.55
+                    //close the servos
+                    if (controller1.rightBumperOnce()){
+                        outTake = OutTake.OUTTAKE_CLOSED
+                    }
+                }
+                OutTake.OUTTAKE_CLOSED ->{
+                    // open the servos
+                    intake.position = 0.55
+                    intake2.position = 0.15
+                    if (controller1.leftBumperOnce()){
+                        outTake = OutTake.OUTTAKE_OPEN
+                    }
+                    // run the motors
+                    if (controller1.rightBumperOnce()){
+                        outTake = OutTake.RUN_FLY_MOTORS
+                    }
+                }
+                OutTake.RUN_FLY_MOTORS ->{
+                    flywheels.maxPower()
+                    flywheels.runMotors()
+                    outTake = OutTake.PUSH_SERVO
+                }
+                OutTake.PUSH_SERVO ->{
+                    if (flywheels.rightFly.motor.velocity>1000 && flywheels.leftFly.motor.velocity>1000){
+                        intake.position = 0.7
+                        timer.reset()
+                        timer.startTime()
+                    }
+                    if (timer.seconds() >0.2){
+                        outTake = OutTake.OUTTAKE_OPEN
+                    }
+
+                }
+
+
+            }
+            /*
             when (outTake) {
                 OutTake.OUTTAKE_START -> {
                     if (controller1.dpad_UpOnce()) {
@@ -43,21 +95,23 @@ class ScrimTele : LinearOpMode(){
 
                 OutTake.PUSH_SERVO -> {
                     if (flywheels.leftFly.motor.velocity > 20 && flywheels.rightFly.motor.velocity > 20) {
-                        // servo stuff
+                        intake.position = 0.7
+                        timer.reset()
+                        timer.startTime()
                         outTake = OutTake.INTAKE_OUT
                     }
                 }
 
                 OutTake.INTAKE_OUT -> {
-
-                    //servo stuff
+                    if (timer.seconds()>0.2){
+                        intake.position = 0.25
+                        intake2.position = 0.55
+                    }
                     outTake = OutTake.OUTTAKE_START
                 }
             }
 
-            drive.stickTankEffort(controller1.right_stick_x, -controller1.left_stick_y)
-            flywheels.runMotors()
-            drive.runMotors()
+             */
             /*
             if (controller1.xOnce()){
                 intake.position = 0.5 // left side
@@ -84,7 +138,7 @@ class ScrimTele : LinearOpMode(){
                 intake2.position -= 0.05
             }
              */
-
+            /*
             //open
             if (controller1.leftBumperOnce()){
                 intake2.position = 0.55
@@ -110,6 +164,8 @@ class ScrimTele : LinearOpMode(){
             } else{
                 flywheels.zeroPower()
             }
+
+             */
 
 
             // open intake2/left is 0.55, intake/right is 0.25
